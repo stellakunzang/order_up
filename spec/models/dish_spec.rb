37849240b8d11e84c1d5c402c7ea9e1ce1a77b43
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-29 09:46:08 -0600

    Create model test for total_calories class method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe "class methods" do
+    it "#total_calories" do
+      louie = Chef.create!(name: "Louis")
+      dinner = Dish.create!(name: "hot dogs", description: "delicious perfect food", chef_id: louie.id)
+      hot_dog = Ingredient.create!(name: "Hebrew National hot dog", calories: 4)
+      bun = Ingredient.create!(name: "bun", calories: 2)
+      DishIngredient.create!(dish_id: dinner.id, ingredient_id: hot_dog.id)
+      DishIngredient.create!(dish_id: dinner.id, ingredient_id: bun.id)
+
+      expect(dinner.total_calories).to eq(6)
+    end
+  end
 end
