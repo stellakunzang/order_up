57bcba9b93161a96f902d937a03eb15766d44192
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-29 09:26:06 -0600

    Setup many to many relationship in models

--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Ingredient, type: :model do
+RSpec.describe DishIngredient do
   describe "relationships" do
     it {should belong_to :dish}
     it {should belong_to :ingredient}
