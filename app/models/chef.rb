ed721688834589c6c1f1777686f53dfaec38ad5d
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-29 10:25:30 -0600

    Add all ingredients method to chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,7 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def all_ingredients
+    dishes.joins(:ingredients).distinct.pluck('ingredients.name').join(', ')
+  end
 end
